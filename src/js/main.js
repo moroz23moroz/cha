c634f6270d3c51e077b322ffa582a419b3840527
Marina Moroz <moroz2387moroz@yandex.ru>
2024-06-04 13:50:12 +1000

    add scroll dependencies

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
+window.addEventListener("DOMContentLoaded", () => {
+  const boxes = document.querySelectorAll(".box");
 
+  const checkBoxes = () => {
+    const triggerBottom = (window.innerHeight / 5) * 4;
+    boxes.forEach((box) => {
+      const boxTop = box.getBoundingClientRect().top;
+
+      if (boxTop < triggerBottom) {
+        box.classList.add("show");
+      } else {
+        box.classList.remove("show");
+      }
+    });
+  };
+
+  window.addEventListener("scroll", checkBoxes);
+});
